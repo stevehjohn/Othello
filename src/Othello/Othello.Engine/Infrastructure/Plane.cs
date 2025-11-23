4fc6d031c15e070445ecfc93f0962c6429170287
Stevo John <stevehuwjohn@gmail.com>
2025-11-23 15:59:17 +0000

    Building out

---
+++ src/Othello/Othello.Engine/Infrastructure/Plane.cs
@@ -0,0 +1,22 @@
+namespace Othello.Engine.Infrastructure;
+
+public struct Plane
+{
+    private ulong _pieces;
+
+    public bool this[int cell]
+    {
+        get => (_pieces & 1ul << (cell - 1)) > 0;
+        set
+        {
+            if (value)
+            {
+                _pieces |= 1ul << (cell - 1);
+            }
+            else
+            {
+                _pieces &= ~(1ul << (cell - 1));
+            }
+        }
+    }
+}
---
+++ src/Othello/Othello.Engine/Infrastructure/Planes.cs
@@ -0,0 +1,10 @@
+using System.Runtime.CompilerServices;
+
+namespace Othello.Engine.Infrastructure;
+
+public struct Planes
+{
+    private Plane _black;
+
+    private Plane _white;
+}
