657289919f2981e4d7b3698ab7a876a9cf80ab9c
Stevo John <stevehuwjohn@gmail.com>
2025-11-23 16:10:48 +0000

    Resharpering

--- src/Othello/Othello.Engine/Infrastructure/Planes.cs
+++ src/Othello/Othello.Engine/Infrastructure/Planes.cs
@@ -1,12 +1,12 @@
 namespace Othello.Engine.Infrastructure;
 
-public struct Planes
+public readonly struct Planes
 {
     public Planes() { }
 
-    public Plane Black { get; private set; } = new();
+    public Plane Black { get; } = new();
 
-    public Plane White { get; private set; } = new();
+    public Plane White { get; } = new();
 
     public bool this[int cell] => Black[cell] | White[cell];
 }
