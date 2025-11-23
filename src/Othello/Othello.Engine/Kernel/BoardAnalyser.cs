0ba1d4439c9d62bdc00b52dbce124b726895f8f3
Stevo John <stevehuwjohn@gmail.com>
2025-11-23 18:05:52 +0000

    Work in progress

--- src/Othello/Othello.Engine/Kernel/BoardAnalyser.cs
+++ src/Othello/Othello.Engine/Kernel/BoardAnalyser.cs
@@ -4,7 +4,9 @@ namespace Othello.Engine.Kernel;
 
 public class BoardAnalyser
 {
-    private Board _board;
+    private static readonly int[] _directions = { -9, -8, -7, -1, 1, 7, 8, 9 };
+    
+    private readonly Board _board;
 
     public BoardAnalyser(Board board)
     {
@@ -13,6 +15,19 @@ public class BoardAnalyser
 
     public ulong GetLegalMoves(Colour colour)
     {
-        return 0;
+        var friendly = colour == Colour.Black ? _board.Black.Pieces : _board.White.Pieces;
+
+        var opponent = colour == Colour.Black ? _board.White.Pieces : _board.Black.Pieces;
+
+        var empty = ~(friendly | opponent);
+
+        var legalMoves = 0ul;
+
+        foreach (var dir in _directions)
+        {
+            legalMoves |= GetLegalMovesInDirection(friendly, opponent, empty, dir);
+        }
+
+        return legalMoves;
     }
 }
