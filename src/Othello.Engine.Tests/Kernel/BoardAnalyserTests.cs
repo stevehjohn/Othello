ad6487a3c7ea8a1a1dded7551c0da36542bdc6db
Stevo John <stevehuwjohn@gmail.com>
2025-11-24 10:41:28 +0000

    Work in progress

--- src/Othello.Engine.Tests/Kernel/BoardAnalyserTests.cs
+++ src/Othello.Engine.Tests/Kernel/BoardAnalyserTests.cs
@@ -14,8 +14,8 @@ public class BoardAnalyserTests
     }
 
     [Theory]
-    [InlineData(Colour.White, 0b0000000000000000000100000010000000000100000010000000000000000000ul)]
-    [InlineData(Colour.Black, 0b0000000000000000000010000000010000100000000100000000000000000000ul)]
+    [InlineData(Colour.Black, 0b0000000000000000000100000010000000000100000010000000000000000000ul)]
+    [InlineData(Colour.White, 0b0000000000000000000010000000010000100000000100000000000000000000ul)]
     public void GetLegalMovesReturnsCorrectMovesForInitialBoardState(Colour colour, ulong expected)
     {
         var board = new Board();
@@ -31,11 +31,11 @@ public class BoardAnalyserTests
         Assert.Equal(expected, moves);
     }
 
-    [Theory]
-    [InlineData(Colour.White,
-        0b0100000011000000000000000000000000000000000000000000000000000000ul,
-        0b1000000000000000000000000000000000000000000000000000000000000000ul,
-        0b0010000000000000101000000000000000000000000000000000000000000000ul)]
+    // [Theory]
+    // [InlineData(Colour.White,
+    //     0b0100000011000000000000000000000000000000000000000000000000000000ul,
+    //     0b1000000000000000000000000000000000000000000000000000000000000000ul,
+    //     0b0010000000000000101000000000000000000000000000000000000000000000ul)]
     // [InlineData(Colour.White,
     //     0b0111111000000000000000000000000000000000000000000000000000000000ul,
     //     0b1000000000000000000000000000000000000000000000000000000000000000ul,
@@ -52,18 +52,18 @@ public class BoardAnalyserTests
     //     0b0000000001000000000000000000000000000000000000000000000000000000ul,
     //     0b0000000010000000000000000000000000000000000000000000000000000000ul,
     //     0b0000000000100000000000000000000000000000000000000000000000000000ul)]
-    public void GetLegalMovesReturnsCorrectMovesForArbitraryBoardState(Colour colour, ulong blackPieces, ulong whitePieces, ulong expected)
-    {
-        var board = new Board();
-        
-        var boardAnalyser = new BoardAnalyser(board);
-
-        board.InitialiseFromBoardState(blackPieces, whitePieces);
-        
-        _outputHelper.WriteLine(board.ToString());
-        
-        var moves = boardAnalyser.GetLegalMoves(colour);
-        
-        Assert.Equal(expected, moves);
-    }
+    // public void GetLegalMovesReturnsCorrectMovesForArbitraryBoardState(Colour colour, ulong blackPieces, ulong whitePieces, ulong expected)
+    // {
+    //     var board = new Board();
+    //     
+    //     var boardAnalyser = new BoardAnalyser(board);
+    //
+    //     board.InitialiseFromBoardState(blackPieces, whitePieces);
+    //     
+    //     _outputHelper.WriteLine(board.ToString());
+    //     
+    //     var moves = boardAnalyser.GetLegalMoves(colour);
+    //     
+    //     Assert.Equal(expected, moves);
+    // }
 }
