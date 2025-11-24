cac9eb088bdc37355f620ff25cc1cf8d092e6d7e
Stevo John <stevehuwjohn@gmail.com>
2025-11-24 11:03:18 +0000

    Noice

--- src/Othello.Engine.Tests/Kernel/BoardTests.cs
+++ src/Othello.Engine.Tests/Kernel/BoardTests.cs
@@ -1,10 +1,19 @@
+using Othello.Engine.Extensions;
 using Othello.Engine.Infrastructure;
 using Othello.Engine.Kernel;
+using Xunit.Abstractions;
 
 namespace Othello.Engine.Tests.Kernel;
 
 public class BoardTests
 {
+    private readonly ITestOutputHelper _outputHelper;
+
+    public BoardTests(ITestOutputHelper outputHelper)
+    {
+        _outputHelper = outputHelper;
+    }
+
     [Fact]
     public void UndoLastMoveRestoresPreviousState()
     {
@@ -12,18 +21,34 @@ public class BoardTests
         
         board.InitialiseNewGame();
         
+        _outputHelper.WriteLine("\nBlack intended move:\n");
+
+        var boardString = board.ToString();
+
+        boardString = boardString.SuperimposeLegalMoves(1ul << 37);
+        
+        _outputHelper.WriteLine(boardString);
+        
         Assert.Equal(0b0000000000000000000000000001000000001000000000000000000000000000ul, board.White.Pieces);
         
         Assert.Equal(0b0000000000000000000000000000100000010000000000000000000000000000ul, board.Black.Pieces);
 
+        _outputHelper.WriteLine("\nBoard state after black move:\n");
+
         board.MakeMove(Colour.Black, 37);
+        
+        _outputHelper.WriteLine(board.ToString());
         
         Assert.Equal(0b0000000000000000000000000000000000001000000000000000000000000000ul, board.White.Pieces);
         
         Assert.Equal(0b0000000000000000000000000011100000010000000000000000000000000000ul, board.Black.Pieces);
         
+        _outputHelper.WriteLine("\nBoard state after undo move:\n");
+
         board.UndoLastMove();
-        
+                
+        _outputHelper.WriteLine(board.ToString());
+
         Assert.Equal(0b0000000000000000000000000001000000001000000000000000000000000000ul, board.White.Pieces);
         
         Assert.Equal(0b0000000000000000000000000000100000010000000000000000000000000000ul, board.Black.Pieces);
