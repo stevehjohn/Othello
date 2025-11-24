2ec1a6065fcd9889fa3fa887d4805f0063827670
Stevo John <stevehuwjohn@gmail.com>
2025-11-24 17:23:25 +0000

    Stuff

--- src/Othello.Console/EntryPoint.cs
+++ src/Othello.Console/EntryPoint.cs
@@ -11,58 +11,63 @@ public static class EntryPoint
 {
     public static void Main()
     {
-        var core = new Core();
-        
-        core.StartGame();
-
-        var ply = 1;
-
-        var player = Colour.White;
-
-        var passCount = 0;
-
-        var stopwatch = new Stopwatch();
-        
-        Clear();
-        
-        while (passCount < 2 && ! core.Board.IsFull)
+        while (! KeyAvailable)
         {
+            var core = new Core();
+
+            core.StartGame();
+
+            var ply = 1;
+
+            var player = Colour.White;
+
+            var passCount = 0;
+
+            var stopwatch = new Stopwatch();
+
+            Clear();
+
+            while (passCount < 2 && ! core.Board.IsFull)
+            {
+                CursorTop = 1;
+
+                WriteLine($" Ply {ply}, player {player}:\n");
+
+                WriteLine($" {core.Board.ToString().Replace("\n", "\n ")}");
+
+                stopwatch.Restart();
+
+                var move = core.GetBestMove(player, 13);
+
+                stopwatch.Stop();
+
+                WriteLine($"\n Thinking time: {stopwatch.ElapsedMilliseconds:N0}ms               ");
+
+                passCount = core.MakeMove(player, move.Cell) ? 0 : passCount + 1;
+
+                player = player.Invert();
+
+                ply++;
+            }
+
+            Clear();
+
             CursorTop = 1;
-            
-            WriteLine($" Ply {ply}, player {player}:\n");
-            
+
+            WriteLine($" Black: {core.Board.BlackScore}, White: {core.Board.WhiteScore}.\n");
+
             WriteLine($" {core.Board.ToString().Replace("\n", "\n ")}");
-            
-            stopwatch.Restart();
 
-            var move = core.GetBestMove(player, 12);
+            if (core.Board.BlackScore == core.Board.WhiteScore)
+            {
+                WriteLine("\n It's a draw.\n");
+            }
+            else
+            {
+                WriteLine($" \n {(core.Board.BlackScore > core.Board.WhiteScore ? "Black" : "White")} wins!\n");
+            }
 
-            stopwatch.Stop();
-            
-            WriteLine($"\n Thinking time: {stopwatch.ElapsedMilliseconds:N0}ms               ");
-
-            passCount = core.MakeMove(player, move.Cell) ? 0 : passCount + 1;
-
-            player = player.Invert();
-
-            ply++;
-        }
-        
-        Clear();
-
-        CursorTop = 1;
-        
-        WriteLine($" Black: {core.Board.BlackScore}, White: {core.Board.WhiteScore}.\n");
-
-        WriteLine($" {core.Board.ToString().Replace("\n", "\n ")}");
-
-        if (core.Board.BlackScore == core.Board.WhiteScore)
-        {
-            WriteLine("\n It's a draw.\n");
-        }
-        else
-        {
-            WriteLine($" \n {(core.Board.BlackScore > core.Board.WhiteScore ? "Black" : "White")} wins!\n");
+            Thread.Sleep(1_000);
         }
     }
 }
