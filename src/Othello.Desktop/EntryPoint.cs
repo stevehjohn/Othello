3eb35c39d68609c3a14ae52df75208d4fa0b2d2d
Stevo John <stevehuwjohn@gmail.com>
2025-11-25 11:36:23 +0000

    Adding UI

---
+++ src/Othello.Desktop/EntryPoint.cs
@@ -0,0 +1,13 @@
+﻿using Othello.Desktop.Presentation;
+
+namespace Othello.Desktop;
+
+public static class EntryPoint
+{
+    public static void Main()
+    {
+        var renderer = new Renderer();
+        
+        renderer.Run();
+    }
+}
---
+++ src/Othello.Desktop/Presentation/Renderer.cs
@@ -0,0 +1,7 @@
+using Microsoft.Xna.Framework;
+
+namespace Othello.Desktop.Presentation;
+
+public class Renderer : Game
+{
+}
