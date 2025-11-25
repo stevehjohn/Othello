138d23fee011ca3d953692c9cb9da7edd1096f35
Stevo John <stevehuwjohn@gmail.com>
2025-11-25 23:14:49 +0000

    Noice

--- src/Othello.Desktop/Presentation/Renderer.cs
+++ src/Othello.Desktop/Presentation/Renderer.cs
@@ -117,10 +117,7 @@ public class Renderer : Game
             _moveTask = null;
         }
         
-        if (_moveTask == null)
-        {
-            _moveTask = Task.Run(() => _bestMove = _core.GetBestMove(_player, 10).Cell);
-        }
+        _moveTask ??= Task.Run(() => _bestMove = _core.GetBestMove(_player, 10).Cell);
 
         base.Update(gameTime);
     }
