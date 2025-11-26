57895927ba4e4a510e1df1dc4a543699b959468c
Stevo John <stevehuwjohn@gmail.com>
2025-11-26 15:24:59 +0000

    Working

--- src/Othello.Desktop/Presentation/Renderer.cs
+++ src/Othello.Desktop/Presentation/Renderer.cs
@@ -2,7 +2,6 @@ using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Othello.Desktop.Orchestration;
-using Othello.Engine.Kernel;
 
 namespace Othello.Desktop.Presentation;
 
